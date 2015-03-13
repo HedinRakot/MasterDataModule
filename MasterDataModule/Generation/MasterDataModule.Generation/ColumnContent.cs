a6a7225bcf6d1ad7ce274315decd80daf2542f2b
HedinRakot <spiridonov.yury@gmail.com>
2015-03-13 03:54:43 +0300

    show in grid

--- MasterDataModule/Generation/MasterDataModule.Generation/ColumnContent.cs
+++ MasterDataModule/Generation/MasterDataModule.Generation/ColumnContent.cs
@@ -6,6 +6,7 @@ namespace MasterDataModule.Generation
     public class ColumnContent : BaseMasterDataContent
     {
         public bool InModel { get; set; }
+        public bool ShowInGrid { get; set; }
         public bool IsModelRequired { get; set; }
         
         public string JsModelType { get; set; }
--- MasterDataModule/MasterDataModuleWeb/web/views/DriverLicenceMasterData/ExamClasses.js
+++ MasterDataModule/MasterDataModuleWeb/web/views/DriverLicenceMasterData/ExamClasses.js
@@ -22,8 +22,6 @@ define([
 		    return [
 				{ field: 'name', title: this.resources.name },
 				{ field: 'description', title: this.resources.description },
-				{ field: 'isMofa', title: this.resources.isMofa , headerTitle: this.resources.isMofa, checkbox: true},
-				{ field: 'isFsClass', title: this.resources.isFsClass , headerTitle: this.resources.isFsClass, checkbox: true},
 				{ field: 'sortOrder', title: this.resources.sortOrder },
 			];
 		}
