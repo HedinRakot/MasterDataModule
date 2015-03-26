21128d44640aa0d96b9624ad1e3611411b8c3988
HedinRakot <spiridonov.yury@gmail.com>
2015-03-26 11:19:47 +0300

    fix solution

--- MasterDataModule/MasterDataModule.Contracts/Enums/MasterDataPermissions.cs
+++ MasterDataModule/MasterDataModule.Contracts/Enums/MasterDataPermissions.cs
@@ -11,29 +11,21 @@ namespace MasterDataModule.Contracts.Enums
     	{
     	 
     		///<summary>
-    		/// Grant for edit SYS_COLUMNS table
+    		/// Grant for edit MASTER_DATA_PERMISSION table
     		///</summary>
-    		public const int SysColumn = 1;
+    		public const int Permission = 1;
     		///<summary>
     		/// Grant for edit MASTER_DATA_ROLE table
     		///</summary>
     		public const int Role = 2;
     		///<summary>
-    		/// Grant for edit MASTER_DATA_PERMISSION table
+    		/// Grant for edit SYS_COLUMNS table
     		///</summary>
-    		public const int Permission = 3;
+    		public const int SysColumn = 3;
     		///<summary>
     		/// Grant for edit SYS_TABLES table
     		///</summary>
     		public const int SysTable = 4;
-    		///<summary>
-    		/// Grant for edit MASTER_DATA_ROLE_PERMISSION_RSP table
-    		///</summary>
-    		public const int MasterDataRolePermissionRsp = 5;
-    		///<summary>
-    		/// Grant for edit MASTER_DATA_USER table
-    		///</summary>
-    		public const int User = 6;
     	}
 }
 
