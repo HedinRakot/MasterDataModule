5b2e08d6cebedd9175e0ddaa591f5c54e589aba4
RockMother <kirill.shumilov@gmail.com>
2015-03-26 14:36:51 +0300

    Fixed permissions enum

--- MasterDataModule/MasterDataModule.Contracts/Enums/MasterDataPermissions.cs
+++ MasterDataModule/MasterDataModule.Contracts/Enums/MasterDataPermissions.cs
@@ -11,21 +11,29 @@ namespace MasterDataModule.Contracts.Enums
     	{
     	 
     		///<summary>
-    		/// Grant for edit MASTER_DATA_PERMISSION table
-    		///</summary>
-    		public const int Permission = 1;
-    		///<summary>
-    		/// Grant for edit MASTER_DATA_ROLE table
-    		///</summary>
-    		public const int Role = 2;
-    		///<summary>
     		/// Grant for edit SYS_COLUMNS table
     		///</summary>
-    		public const int SysColumn = 3;
+    		public const int SysColumn = 1;
     		///<summary>
     		/// Grant for edit SYS_TABLES table
     		///</summary>
-    		public const int SysTable = 4;
+    		public const int SysTable = 2;
+    		///<summary>
+    		/// Grant for edit MASTER_DATA_ROLE table
+    		///</summary>
+    		public const int Role = 3;
+    		///<summary>
+    		/// Grant for edit MASTER_DATA_PERMISSION table
+    		///</summary>
+    		public const int Permission = 4;
+    		///<summary>
+    		/// Grant for edit MASTER_DATA_ROLE_PERMISSION_RSP table
+    		///</summary>
+    		public const int MasterDataRolePermissionRsp = 5;
+    		///<summary>
+    		/// Grant for edit MASTER_DATA_USER table
+    		///</summary>
+    		public const int User = 6;
     	}
 }
 
