630a9689726ea75b73e944a4bee29cba6b994da1
RockMother <kirill.shumilov@gmail.com>
2015-03-27 18:29:44 +0300

    For prevous commit

---
+++ MasterDataModule/MasterDataModuleWeb/web/views/settings/Custom.SysTable.js
@@ -0,0 +1,13 @@
+﻿define([
+	'lr!resources/Settings/Custom.SysTables',
+], function (Resources) {
+    'use strict';
+
+    var extraColumns = function () {
+        return [
+            { field: 'tableDescription', title: Resources.tableDescription, filterable: false }
+        ];
+    };
+
+    return extraColumns;
+});
