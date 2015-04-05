05982992af4f0763095f378ef7fbc1c70e9862d8
RockMother <kirill.shumilov@gmail.com>
2015-04-05 18:31:30 +0300

    Added summary

--- MonitoringAgent/MonitoringAgent.Data.Interfaces/Enums/CheckModuleType.cs
+++ MonitoringAgent/MonitoringAgent.Data.Interfaces/Enums/CheckModuleType.cs
@@ -1,10 +1,25 @@
 namespace MonitoringAgent.Data.Interfaces.Enums
 {
+    /// <summary>
+    /// Type of module
+    /// </summary>
     public enum CheckModuleType
     {
+        /// <summary>
+        /// Default value
+        /// </summary>
         None = 0,
+        /// <summary>
+        /// Wcf services
+        /// </summary>
         WcfService = 1,
+        /// <summary>
+        /// Sites
+        /// </summary>
         Site = 2,
+        /// <summary>
+        /// Jobs
+        /// </summary>
         Job = 3
     }
 }
