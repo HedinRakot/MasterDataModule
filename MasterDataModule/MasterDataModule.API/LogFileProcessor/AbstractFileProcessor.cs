79fc4458042adaa177ae39c10f975b3a32c37a4e
aopritov <hutte_spb@mail.ru>
2015-04-06 10:59:49 +0300

    Added parser for FEServiceMAin log.

---
+++ MasterDataModule/MasterDataModule.API/LogFileProcessor/AbstractFileProcessor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using MasterDataModule.Contracts.Entities.Configuration;
+using MasterDataModule.Lib.Managers.Configuration;
+
+namespace MasterDataModule.API.LogFileProcessor
+{
+    /// <summary>
+    /// Abstract file processor class
+    /// </summary>
+    public abstract class AbstractFileProcessor
+    {
+        /// <summary>
+        /// Loads file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private IReadOnlyCollection<string> LoadFile(String path)
+        {
+            var result = new List<string>();
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+            try
+            {
+
+                using (var file = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            result.Add(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format(CultureInfo.InvariantCulture, "Can not read file {0}: {1}", path, ex.Message), ex);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Processes content to entities
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        protected abstract List<ApplicationLogs> ProcessData(IReadOnlyCollection<string> content);
+
+        /// <summary>
+        /// Exports entities to storage
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="manager"></param>
+        public void Export(String path, ApplicationLogsManager manager)
+        {
+            var content = LoadFile(path);
+            var entites = ProcessData(content);
+            entites.ForEach(manager.AddEntity);
+            manager.SaveChanges();
+        }
+    }
+}
---
+++ MasterDataModule/MasterDataModule.API/LogFileProcessor/LogFileProcessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using MasterDataModule.Contracts.Entities.Configuration;
+using MasterDataModule.Contracts.Enums;
+
+namespace MasterDataModule.API.LogFileProcessor
+{
+    /// <summary>
+    /// Implementation of file processor fo logs files
+    /// </summary>
+    public class LogFileProcessor : AbstractFileProcessor
+    {
+        const string Pattern =
+            @"^=== (?<type>.*)\t(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})\s" +
+            @"(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})" +
+            @"(?<useless1>.{4,12})(?<text>.+)";
+
+       protected override List<ApplicationLogs> ProcessData(IReadOnlyCollection<string> content)
+        {
+            var entities = new List<ApplicationLogs>();
+           foreach (var line in content)
+            {
+                //try to macth the line using regular expressions
+                var parsed = Regex.Match(line, Pattern);
+                if (parsed.Success)
+                {
+                    entities.Add(CreateEntity(parsed));
+                }
+            }
+            return entities.Where(e => e != null).ToList();
+        }
+
+        private ApplicationLogs CreateEntity(Match parsed)
+        {
+            var entity = new ApplicationLogs();
+            var logLevel = GetLogLevel(parsed.Groups["type"].Value);
+            if (logLevel == LogLevelEnum.None)
+            {
+                return null;
+            }
+            entity.LogLevel = (int)logLevel;
+            entity.LogType = (int)LogTypeEnum.FeServiceMain;
+            entity.Date = CreateDate(parsed);
+            entity.Message = parsed.Groups["text"].Value;
+            return entity;
+        }
+
+        private LogLevelEnum GetLogLevel(String logLevel)
+        {
+            LogLevelEnum e;
+            if (!Enum.TryParse(logLevel, true, out e))
+            {
+                e = LogLevelEnum.None;
+            }
+            return e;
+        }
+
+        private static DateTime? CreateDate(Match parsed)
+        {
+            try
+            {
+                var year = Convert.ToInt32(parsed.Groups["year"].Value);
+                var month = Convert.ToInt32(parsed.Groups["month"].Value);
+                var day = Convert.ToInt32(parsed.Groups["day"].Value);
+                var hour = Convert.ToInt32(parsed.Groups["hour"].Value);
+                var minute = Convert.ToInt32(parsed.Groups["minute"].Value);
+                var second = Convert.ToInt32(parsed.Groups["second"].Value);
+                return new DateTime(year, month, day, hour, minute, second);
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+            catch (OverflowException)
+            {
+                return null;
+            }
+        }
+    }
+}
