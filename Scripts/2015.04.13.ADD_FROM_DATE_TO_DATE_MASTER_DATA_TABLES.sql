ebafdd1f4db039fdde2e9a897e22165b616e372f
RockMother <kirill.shumilov@gmail.com>
2015-04-13 13:10:26 +0300

    Updated scripts for triggers and added fromdate to date colums to mster data database tables

---
+++ Scripts/2015.04.13.ADD_FROM_DATE_TO_DATE_MASTER_DATA_TABLES.sql
@@ -0,0 +1,89 @@
+ALTER TABLE dbo.APPLICATION_LOGS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_JOB_CHECK_RESULTS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_JOB_INFO ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_MONITOR_STATE ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_MONITORABLE_INFO_MASTER_DATA_NOTIFICATIONS_RSP ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_NOTIFICATIONS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_NOTIFICATIONS_MASTER_DATA_SUBSCRIBERS_RSP ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_SITE_CHECK_RESULTS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_SITE_INFO ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_SUBSCRIBERS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_WCF_CHECK_RESULTS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_WCF_INFO ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_WINDOWS_SERVICE_CHECK_RESULTS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.MASTER_DATA_WINDOWS_SERVICE_INFO ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.SYS_COLUMNS ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.SYS_TABLES ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+ALTER TABLE dbo.SYS_DB_VERSION ADD FROM_DATE datetime2(2) NOT NULL DEFAULT(GETDATE()), TO_DATE datetime2(2) NOT NULL DEFAULT(DATEADD(year, 1, GETDATE()));
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'APPLICATION_LOGS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'APPLICATION_LOGS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_JOB_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_JOB_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_JOB_INFO', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_JOB_INFO', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_MONITOR_STATE', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_MONITOR_STATE', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_MONITORABLE_INFO_MASTER_DATA_NOTIFICATIONS_RSP', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_MONITORABLE_INFO_MASTER_DATA_NOTIFICATIONS_RSP', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_NOTIFICATIONS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_NOTIFICATIONS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_NOTIFICATIONS_MASTER_DATA_SUBSCRIBERS_RSP', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_NOTIFICATIONS_MASTER_DATA_SUBSCRIBERS_RSP', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_SITE_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_SITE_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_SITE_INFO', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_SITE_INFO', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_SUBSCRIBERS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_SUBSCRIBERS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WCF_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WCF_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WCF_INFO', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WCF_INFO', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WINDOWS_SERVICE_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WINDOWS_SERVICE_CHECK_RESULTS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WINDOWS_SERVICE_INFO', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MASTER_DATA_WINDOWS_SERVICE_INFO', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SYS_COLUMNS', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SYS_COLUMNS', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SYS_TABLES', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SYS_TABLES', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Von Datum  EN:  Date from', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SYS_DB_VERSION', @level2type=N'COLUMN', @level2name=N'FROM_DATE';
+
+EXEC [sys].[sp_addextendedproperty] @name = N'MS_Description', @value = N'DE:Bis Datum  EN:  Date to', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SYS_DB_VERSION', @level2type=N'COLUMN', @level2name=N'TO_DATE';
+
+
+
+
--- Scripts/2015.04.13.MASTER_DATA_CHANGE_TRACKER_TRIGGER.sql
+++ Scripts/2015.04.13.MASTER_DATA_CHANGE_TRACKER_TRIGGER.sql
@@ -1,11 +1,12 @@
+/****** Object: Server Trigger [SETTINGS_DATA_SYS_TABLES_FILL_TRIGGER]   Script Date: 13.04.2015 12:46:45 ******/
 USE [master];
 GO
-SET ANSI_NULLS ON;
+IF EXISTS(SELECT * FROM SYS.ALL_OBJECTS WHERE [name] = 'SETTINGS_DATA_SYS_TABLES_FILL_TRIGGER')
+  DROP TRIGGER [SETTINGS_DATA_SYS_TABLES_FILL_TRIGGER] ON ALL SERVER;
 GO
-SET QUOTED_IDENTIFIER ON;
-GO
-ALTER TRIGGER [SETTINGS_DATA_SYS_TABLES_FILL_TRIGGER]
+CREATE TRIGGER [SETTINGS_DATA_SYS_TABLES_FILL_TRIGGER]
 ON ALL SERVER
+WITH EXEC AS CALLER
 FOR DDL_TABLE_EVENTS
 AS
 BEGIN
@@ -25,6 +26,8 @@ SELECT @eventType = @data.value('(/EVENT_INSTANCE/EventType)[1]', 'nvarchar(100)
 IF (@eventType = 'ALTER_TABLE')
 BEGIN
 SELECT TOP(1) @tableId = ID FROM SettingsData.dbo.SYS_TABLES WHERE NAME = @tableName;
+IF (@tableId IS NOT NULL)
+BEGIN
 INSERT INTO SettingsData..SYS_COLUMNS (
    SYS_TABLE_ID
   ,[NAME]
@@ -34,6 +37,7 @@ FROM  @data.nodes('/EVENT_INSTANCE/AlterTableActionList/Create/Columns/Name') t(
 DELETE FROM SettingsData..SYS_COLUMNS 
 WHERE SYS_TABLE_ID = @tableId AND NAME IN 
 (SELECT t.n.value('.', 'sysname') FROM  @data.nodes('/EVENT_INSTANCE/AlterTableActionList/Drop/Columns/Name') t(n));
+END
 END
 IF (@eventType = 'DROP_TABLE')
 BEGIN
@@ -61,6 +65,7 @@ SET @query = CONCAT('INSERT INTO SettingsData.dbo.SYS_COLUMNS (SYS_TABLE_ID, [NA
      LEFT JOIN ', @database, '.sys.extended_properties ep ON ep.major_id = t.[object_id] AND ep.minor_id = c.column_id AND ep.[name] = ''MS_Description'') AND t.name = ''', @tableName, '''');
 END
 END;
-
 GO
 ENABLE TRIGGER [SETTINGS_DATA_SYS_TABLES_FILL_TRIGGER] ON ALL SERVER;
+GO
+
