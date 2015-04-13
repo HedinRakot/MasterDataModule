cd3b33a3d416b68498c6b264ea57916b9513d05b
RockMother <kirill.shumilov@gmail.com>
2015-04-13 13:46:11 +0300

    SignalR added to project

---
+++ MasterDataModule/MasterDataModule.API/SignalR/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace MasterDataModule.API.SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
