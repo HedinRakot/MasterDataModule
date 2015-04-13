f4bc63abf925f7a93bde5d56a01b2838f823be3d
RockMother <kirill.shumilov@gmail.com>
2015-04-13 15:00:50 +0300

    Added test hub classes

---
+++ MasterDataModule/MasterDataModule.API/SignalR/MonitorableObjectsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace MasterDataModule.API.SignalR
+{
+    [HubName("monitorableObjects")]
+    public class MonitorableObjectsHub: Hub
+    {
+        public void StatusChanged(string moduleType, int result, string message, string name)
+        {
+
+        }
+    }
+}
---
+++ MonitoringAgent/MonitoringAgent.Services.Common/SignalRNotification/HubNotificator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using MonitoringAgent.Data.Interfaces.Enums;
+
+namespace MonitoringAgent.Services.Common.SignalRNotification
+{
+    public class HubNotificator
+    {
+        private bool already = false;
+        public void SendNotification(CheckModuleType type, int result, string message, string name)
+        {
+            if (already)
+                return;
+            already = true;
+
+            var connection = new HubConnection("http://localhost:49991/");
+            var myHub = connection.CreateHubProxy("monitorableObjects");
+
+            connection.Start().Wait(); // not sure if you need this if you are simply posting to the hub
+
+            myHub.Invoke("StatusChanged", type, result, message, name); 
+        }
+    }
+}
